2d5f17e2ee1e3173fa5dd23ee2339031b847870e
Ievgennii <simonovievgenii@gmail.com>
2023-03-24 20:05:19 +0200

    add clear btn

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const galeryFormEl = document.querySelector('#search-form');
 const galleryListEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
+const clearAllBtnEl = document.querySelector('.clear-all');
 
 galeryFormEl.addEventListener('submit', handleSearchGallery);
 loadMoreBtnEl.addEventListener('click', handleLoadMoreBtnClick);
+clearAllBtnEl.addEventListener('click', clearAll);
 
 let options = {
   params: {
@@ -28,6 +30,8 @@ function handleSearchGallery(event) {
   options.params.q = event.target.elements.searchQuery.value.trim();
   options.params.page = 1;
   galleryListEl.innerHTML = '';
+  clearAllBtnEl.classList.add('is-hidden');
+  loadMoreBtnEl.classList.add('is-hidden');
 
   fetchGallery(options)
     .then(function ({ data }) {
@@ -37,8 +41,9 @@ function handleSearchGallery(event) {
       Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
 
       addImagesGallery(data);
-
+if (data.totalHits > options.params.per_page) {
       loadMoreBtnEl.classList.remove('is-hidden');
+    } else {clearAllBtnEl.classList.remove('is-hidden');}
       // galeryFormEl.reset();
       // smoothScroll()
     })
@@ -47,6 +52,7 @@ function handleSearchGallery(event) {
       Notiflix.Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      
     })
     .finally(() => {
       galeryFormEl.reset();
@@ -67,10 +73,13 @@ function handleLoadMoreBtnClick() {
     })
     .catch(() => {
       loadMoreBtnEl.classList.add('is-hidden');
+      clearAllBtnEl.classList.remove('is-hidden');
+      
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
       );
       options.params.page = 1;
+      clearAllBtnEl.classList.remove('is-hidden');
     });
 }
 
@@ -79,6 +88,11 @@ function addImagesGallery(data) {
   galleryListEl.insertAdjacentHTML('beforeend', addImagesGalery);
   let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
   gallery.on('show.simplelightbox');
+}
+
+function clearAll(){
+  galleryListEl.innerHTML = '';
+  clearAllBtnEl.classList.add('is-hidden');
 }
 
 function smoothScroll() {
